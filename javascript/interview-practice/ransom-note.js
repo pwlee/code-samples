5962c610b169ce992150062a5119ba84d9186cd7
Peter Lee <petey525@gmail.com>
2019-04-10 21:59:12 -0400

    Add problem link to ransom note problem

--- javascript/interview-practice/ransom-note.js
+++ javascript/interview-practice/ransom-note.js
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/ransom-note
+
 // Given an arbitrary ransom note string and another string containing letters 
 // from all the magazines, write a function that will return true if the ransom
 // note can be constructed from the magazines ; otherwise, it will return false.
