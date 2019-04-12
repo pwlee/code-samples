43d3327d464ec8e0eb6a50ee5446e6dc078b4ce8
Peter Lee <petey525@gmail.com>
2019-04-12 01:00:00 -0400

    Add solution to "two sum" problem

---
+++ javascript/interview-practice/leet-code/001-two-sum.js
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/two-sum
+
+// Given an array of integers, return indices of the two numbers such that they
+// add up to a specific target.
+// 
+// You may assume that each input would have exactly one solution, and you may
+// not use the same element twice.
+// 
+// Example:
+// 
+// Given nums = [2, 7, 11, 15], target = 9,
+// 
+// Because nums[0] + nums[1] = 2 + 7 = 9,
+// return [0, 1].
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const mappedValues = {}
+
+    // map each value to its index
+    // { 2: 0, 7: 1, 11: 2, 15: 3 }
+    for (let i = 0; i < nums.length; i++) {
+        mappedValues[nums[i]] = i;
+    }
+    
+    for (let i = 0; i < nums.length; i++) {
+        const difference = target - nums[i]
+        if (mappedValues[difference] && mappedValues[difference] !== i) {
+            return [i, mappedValues[difference]]
+        }
+    }
+    
+    return []
+};
