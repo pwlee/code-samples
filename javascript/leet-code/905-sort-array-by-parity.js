52c980cf98b547943a36615bb56443263950646e
Peter Lee <petey525@gmail.com>
2019-06-08 23:04:18 -0400

    Add solution to 'Sort Array By Parity' problem

---
+++ javascript/leet-code/905-sort-array-by-parity.js
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/sort-array-by-parity/
+//
+// Given an array A of non-negative integers, return an array consisting of all
+// the even elements of A, followed by all the odd elements of A.
+// 
+// You may return any answer array that satisfies this condition.
+// 
+// 
+// 
+// Example 1:
+// 
+// Input: [3,1,2,4]
+// Output: [2,4,3,1]
+// The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+const sortArrayByParity = (A) => {
+  const evens = []
+  const odds = []
+  
+  A.forEach((val) => {
+    const bucket = val % 2 === 0 ? evens : odds
+    bucket.push(val)
+  })
+  
+  return evens.concat(odds)
+}
